eaab756840b721bc7db0776c28337774302b8d3c
smule29 <sreiner1@sas.upenn.edu>
2026-02-07 22:07:23 -0500

    renamed dataframe

--- scripts/02_analysis.R
+++ scripts/02_analysis.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 
 # Converting to relative annual wages (dividing by median)
-complete_add_relative <- complete |>
+complete_final <- complete |>
   group_by(season) |>
   mutate(
     relative_annual_wages = annual_wages_gbp / median(annual_wages_gbp, na.rm = TRUE)
@@ -9,11 +9,12 @@ complete_add_relative <- complete |>
   ungroup()
 
 # Regression #1: Simple regression, points vs relative wages
-naive_regression <- lm(points ~ relative_annual_wages, data = complete_add_relative)
+regression_1 <- lm(points ~ relative_annual_wages, data = complete_final)
 # Plotting data and regression line
-ggplot(data = complete_add_relative, mapping = aes(x= relative_annual_wages, y = points)) +
+ggplot(data = complete_final, mapping = aes(x= relative_annual_wages, y = points)) +
   geom_point() +
   geom_smooth(method = "lm") +
   labs(
     title = "Points vs Relative Annual Wages in the Premier League, 2014-2025"
   )
+
