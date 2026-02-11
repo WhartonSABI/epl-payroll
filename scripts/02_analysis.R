bbc107c0aa6cac3380e9e13bd080277b7fca9f28
smule29 <sreiner1@sas.upenn.edu>
2026-02-11 14:31:19 -0500

    Script for all 4 regressions

--- scripts/02_analysis.R
+++ scripts/02_analysis.R
@@ -1,6 +1,6 @@
 library(tidyverse)
 
-# Converting to relative annual wages (dividing by median)
+# converting to relative payroll
 complete_final <- complete |>
   group_by(season) |>
   mutate(
@@ -8,13 +8,37 @@ complete_final <- complete |>
   ) |>
   ungroup()
 
-# Regression #1: Simple regression, points vs relative wages
+# regression 1: E[points] vs relative wages
 regression_1 <- lm(points ~ relative_annual_wages, data = complete_final)
-# Plotting data and regression line
+
 ggplot(data = complete_final, mapping = aes(x= relative_annual_wages, y = points)) +
   geom_point() +
   geom_smooth(method = "lm") +
   labs(
     title = "Points vs Relative Annual Wages in the Premier League, 2014-2025"
   )
+  
+  
+# regression 2: E[Points] vs wages, team as factor
+regression_2 <- lm(points ~ relative_annual_wages + team, data = complete_final)
+
+ggplot(data = complete_final, mapping = aes(x = relative_annual_wages, 
+                                             y = points, color = team)) +
+  geom_point() +
+  geom_smooth(method = "lm", se = FALSE) +
+  coord_cartesian(xlim = c(0, 4), ylim = c(0,100))
+
+# regression 3: simple E[wages] vs points
+regression_3 <- lm(relative_annual_wages ~ points, data = complete_final)
+
+ggplot(data = complete_final, mapping = aes(
+  x = points, y = relative_annual_wages
+  )) +
+  geom_point() + 
+  geom_smooth(method = "lm", se = FALSE)
+
+
+# regression 4: E[wages] vs points, team as factor
+regression_4 <- lm(relative_annual_wages ~ points + team, data = complete_final)
+
 
